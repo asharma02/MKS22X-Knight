ba3eae4baae3a9074059dc215b71adf2d9ce557e
Aryan Sharma <asharma00@stuy.edu>
2019-02-14 09:49:02 -0500

    solve, started helper solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,6 +4,9 @@ public class KnightBoard {
   /**@throws IllegalArgumentException when either parameter is negative. **/
 
 public KnightBoard(int startingRows,int startingCols) {
+  if (startingRows < 0 || startingCols < 0) {
+    throw new IllegalArgumentException();
+  }
   board = new int[startingRows][startingCols];
   for (int r = 0 ; r < startingRows ; r++) {
     for (int c = 0 ; c < startingCols; c++) {
@@ -29,6 +32,7 @@ public String toString() {
   return output;
 }
 
+
 /**blank boards display 0's as underscores
 you get a blank board if you never called solve or
 when there is no solution**/
@@ -38,16 +42,44 @@ when there is no solution**/
 @throws IllegalArgumentException when either parameter is negative
  or out of bounds. **/
 
-public boolean solve(int startingRow, int startingCol){}
+public boolean solve(int startingRow, int startingCol){
+  for(int r = 0; r < board.length; r++) {
+    for(int c = 0; c < board[0].length; c++) {
+      if(board[r][c] != 0) {
+        throw new IllegalStateException();
+      }
+    }
+  }
+  //CHECK IF THE BOARD CONTAINS NONZERO VALUES
+  if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) {
+    throw new IllegalArgumentException();
+  }
+  //CHECK IF EITHER PARAMETER IS NEGATIVE OR OUT OF bounds
+  return solveH(0,0, 1);
+}
 
 /**
 @throws IllegalStateException when the board contains non-zero values.
 @throws IllegalArgumentException when either parameter is negative
  or out of bounds. **/
-public int countSolutions(int startingRow, int startingCol){}
+public int countSolutions(int startingRow, int startingCol){
+  return 0;
+}
 
 //Suggestion:
-private boolean solveH(int row ,int col, int level){}
-// level is the # of the knight
+private boolean solveH(int row ,int col, int level){
+  if (row > 0 && row < board.length && col > 0 && col < board[0].length) { //chekcs if the row recursively returned is in the board
+    if (level == startingRows * startingCols) {
+      return true;
+    } //if all squares visited, print solution
+    //choose one of the 8 moves and recursively check if this leads to the solution
+    // if move doesnt work, try an alternative move
+    //if no paths work, return false, will cause to remove the previously added item in recursion
+      //if false is returned by the inital call, the method will return false
+  }
+  else {
+    return false;
+  }
+}
 
 }
